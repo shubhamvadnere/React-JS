784eaf0ca7c0e30e67008d3afc825d33ecb365d2
Shubham Vadnere <vadnereshubham@gmail.com>
2022-10-23 12:58:58 +0530

    Add Part 1

---
+++ ReduxCart/src/components/store/cart-slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cart = createSlice({
+    name: "cart",
+    initialState:{
+        items:[],
+        totalQuantity: 0,
+    },
+    reducers: {
+        addItemToCart(state, action){
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            if (!existingItem) {
+                state.items.push({
+                    itemID: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title,
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice += newItem.price;
+            }
+        },
+        removeItemFromCart(){
+
+        }
+    }
+
+});
